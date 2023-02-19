fb4d7f4fc1f63f78f40d77b9e9793dda88ff020a
cshu401 <cshu401@aucklanduni.ac.nz>
2023-02-20 04:24:32 +1300

    basic structure implemented

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+
+
+
 url = "https://steamcommunity.com/market/search?appid=730&q=case+key#p1_default_desc"
 
 response = requests.get(url)
 
-soup = BeautifulSoup(response.text)
+soup = BeautifulSoup(response.text, features="html.parser")
 # print(soup, type(soup), sep= '\n')
 
-# variable = soup.find("span", {"class": "market_listing_item_name"})
 
-retrievedOb = soup.find_all("div", "market_listing_row market_recent_listing_row market_listing_searchresult")
+#Gets all listings
+list = soup.find_all("div", "market_listing_row market_recent_listing_row market_listing_searchresult")
+#Gets amount of listings
+listLen = len(list)
 
-\
+lowestprice = list[0].find(class_ = "sale_price")
+highestprice = list[0].find(class_ = "normal_price")
 
-print(retrievedOb[1])
+
+
+
+print(list[0])
+print("===============================")
+print("Lowest Price:" + str(lowestprice))
+print("Highest Price:" + str(highestprice))
 
