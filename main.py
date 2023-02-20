944c13011863f2682ca6866079c7657372453100
cshu401 <cshu401@aucklanduni.ac.nz>
2023-02-21 01:34:34 +1300

    highestPriceExtractor, lowestPriceExtractor implemented

--- main.py
+++ main.py
@@ -2,6 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 
 
+'''
+    @param inList - input single list
+    @return value in string of highest price
+'''
+def highestPriceExtractor(inList):
+    highestpriceSoup = inList.find(class_="normal_price")
+    highestpriceSoup = str(highestpriceSoup)
+    print(highestpriceSoup)
+    highestpriceloc1 = highestpriceSoup.find("data-price")
+    highestpriceloc2 = highestpriceSoup.find("span>")
+    highestprice = highestpriceSoup[highestpriceloc1:highestpriceloc2]
+    highestpriceloc21 = highestprice.find(">") + 1
+    highestpriceloc22 = highestprice.find("</")
+    highestprice2 = highestprice[highestpriceloc21:highestpriceloc22]
+    return highestprice2
+
+def lowestPriceExtractor(inList):
+    lowestpriceSoup = inList.find(class_="sale_price")
+    for lowestpriceSoup in lowestpriceSoup:
+        print(lowestpriceSoup)
+
 
 
 
@@ -13,19 +34,35 @@ soup = BeautifulSoup(response.text, features="html.parser")
 # print(soup, type(soup), sep= '\n')
 
 
-#Gets all listings
+#list - all listings of page extracted
 list = soup.find_all("div", "market_listing_row market_recent_listing_row market_listing_searchresult")
 #Gets amount of listings
 listLen = len(list)
+#gets name of listings
 
-lowestprice = list[0].find(class_ = "sale_price")
-highestprice = list[0].find(class_ = "normal_price")
+
+name = list[0].get("data-hash-name")
+
+#extracts lowest price
+lowestprice = lowestPriceExtractor(list[0])
+# lowestpriceSoup = list[0].find(class_ = "sale_price")
+# for lowestpriceSoup in lowestpriceSoup:
+#     print(lowestpriceSoup)
+
+#extracts highest price
+highestprice = highestPriceExtractor(list[0])
+
+
+
+
 
 
 
 
 print(list[0])
 print("===============================")
+print("Name:" + str(name))
 print("Lowest Price:" + str(lowestprice))
 print("Highest Price:" + str(highestprice))
+
 
